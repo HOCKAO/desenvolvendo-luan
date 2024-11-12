32dd65b688079e4545ceaf60f9bf37dd6a7f068f
Luan Machado <142520449+HOCKAO@users.noreply.github.com>
2024-11-11 22:00:38 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Cabelo',
+    'QUal corte de cabelo mais famoso?',
+    'Low fade'
+)
+
+criaCartao(
+    'Capital',
+    'Qual a capital do Paraná?',
+    'Curitiba'
+)
+
+criaCartao(
+    'Copa Do Mundo',
+    'Quem ganhou a copa de 2022?',
+    'Argentina'
+)
+
+criaCartao(
+    'Futebol',
+    'Qual melhor jogador do mundo?',
+    'Pelé'
+)
